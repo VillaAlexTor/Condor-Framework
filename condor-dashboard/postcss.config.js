c8eafa81a3129279aef27642a9000980ff7158a8
villaAlexTor <153693343+VillaAlexTor@users.noreply.github.com>
2026-06-16 12:29:33 -0400

    Actualizacion condor-dashboard/postcssconfig

--- condor-dashboard/postcss.config.js
+++ condor-dashboard/postcss.config.js
@@ -1,12 +1,3 @@
-/**
- * ╔══════════════════════════════════════════════════════╗
- * ║  CÓNDOR FRAMEWORK — condor-dashboard/postcss.config.js║
- * ╚══════════════════════════════════════════════════════╝
- *
- * PostCSS procesa el CSS antes de que Vite lo sirva.
- * Tailwind necesita este archivo para inyectar sus utilidades.
- */
-
 export default {
   plugins: {
     tailwindcss: {},
